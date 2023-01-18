b093d281320b15066476a7925c6e7a4099c914f8
Mikolaj Gasior <mikolaj@gasior.dev>
2023-01-18 11:52:45 +0100

    Tweak the create feature env script

--- create-feature-env.sh
+++ create-feature-env.sh
@@ -11,7 +11,7 @@ if [[ ! -d featureenvs/$1 ]]; then
   git submodule add https://github.com/mikogs/project-template-apps-node.git node
   cd ../../
 else
-  git submodule update featureenvs/$1/node
+  git submodule update --remote featureenvs/$1/node
 fi
 
 cd featureenvs/$1
