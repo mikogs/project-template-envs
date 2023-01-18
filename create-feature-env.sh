ecbf594aa422501425dfc3d71c00657f8bad78cc
Mikolaj Gasior <mikolaj@gasior.dev>
2023-01-18 05:08:14 +0100

    Add paula-boss feature env

---
+++ create-feature-env.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ "$1" == "" ]]; then
+  echo "Syntax: create-feature-env.sh ENV_NAME"
+  exit 1
+fi
+
+mkdir -p featureenvs/$1
+cd featureenvs/$1
+git submodule add https://github.com/mikogs/project-template-apps-node.git node
+cp ../template/argocd-app.yaml .
+cp ../template/kustomization.yaml .
+sed -i "s|FEATURE_ENV_NAME|$1|g" "argocd-app.yaml"
+sed -i "s|FEATURE_ENV_NAME|$1|g" "kustomization.yaml"
