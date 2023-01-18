8506a2c1fccd9aab4396610e287022fc33fb20af
Mikolaj Gasior <mikolaj@gasior.dev>
2023-01-18 21:37:43 +0100

    Add frontend

--- create-feature-env.sh
+++ create-feature-env.sh
@@ -9,9 +9,11 @@ if [[ ! -d featureenvs/$1 ]]; then
   mkdir -p featureenvs/$1
   cd featureenvs/$1
   git submodule add https://github.com/mikogs/project-template-apps-node.git node
+  git submodule add https://github.com/mikogs/project-template-apps-frontend.git frontend
   cd ../../
 else
   git submodule update --remote featureenvs/$1/node
+  git submodule update --remote featureenvs/$1/frontend
 fi
 
 cd featureenvs/$1
