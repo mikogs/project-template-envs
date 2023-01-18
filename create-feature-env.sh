9622bd4df2e3b500a6ff637a0b2f4c954c1bd953
Mikolaj Gasior <mikolaj@gasior.dev>
2023-01-18 11:51:28 +0100

    Tweak the create-feature-env script

--- create-feature-env.sh
+++ create-feature-env.sh
@@ -5,8 +5,15 @@ if [[ "$1" == "" ]]; then
   exit 1
 fi
 
-mkdir -p featureenvs/$1
+if [[ ! -d featureenvs/$1 ]]; then
+  mkdir -p featureenvs/$1
+  cd featureenvs/$1
+  git submodule add https://github.com/mikogs/project-template-apps-node.git node
+  cd ../../
+else
+  git submodule update featureenvs/$1/node
+fi
+
 cd featureenvs/$1
-git submodule add https://github.com/mikogs/project-template-apps-node.git node
 cp ../template/kustomization.yaml .
 sed -i "s|FEATURE_ENV_NAME|$1|g" "kustomization.yaml"
